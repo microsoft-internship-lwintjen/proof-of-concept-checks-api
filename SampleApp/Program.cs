b053e260631bd53591b6e50ca7a9f641d82cd404
lwintjen <loris.wintjens@gmail.com>
2020-07-01 19:06:12 +0200

    implementing octokit package to get the given org + repo (next steps are to ask for a PR then create a check for it)

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,22 +1,29 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Octokit;
 namespace SampleApp
 {
     class Program
     {
-
+        static Repository findRepository(string repoName, IReadOnlyList<Repository> repositories)
+        {
+            foreach (var repo in repositories)
+            {
+                if (repo.Name == repoName)
+                {
+                    return repo;
+                }
+            }
+            return null;
+        }
 
         static async Task Main(string[] args)
         {
             var client = new GitHubClient(new ProductHeaderValue("SampleApp"));
             //github.Check.Suite.Create()
             string tokenPath = @"C:\Windows\System\.env";
-
-            string login = "lwintjen";
 
             DotNetEnv.Env.Load(tokenPath);
             try
@@ -36,16 +43,47 @@ namespace SampleApp
 
                 var user = await client.User.Current();
 
-                var repoList = await client.Organization.GetAllForUser(login);
+                Console.Write("Please, enter the name of the organization you want to find the PR in : ");
+                string orgName = Console.ReadLine();
 
-                for (int i = 0; i < repoList.Count; i++)
+                var org = await client.Organization.Get(orgName);
+                var repoOrg = await client.Repository.GetAllForOrg(org.Login);
+
+
+                // If there are 0 repo in the org, then there's no point running this program
+                if (repoOrg.Count == 0) 
                 {
-                    Console.WriteLine(repoList.ElementAt(i));
+                    Console.WriteLine("The given organization has no repository in it, please create a repo before trying to use this program.");
+                    Environment.Exit(0);
                 }
-                Console.WriteLine("user : " + repoList);
 
 
-            } catch (FileNotFoundException e)
+                Console.Write("Please, enter the name of the repository you want to find the PR in (the repo has to be in your organization) : ");
+                string repoName = Console.ReadLine();
+
+                Repository repo = findRepository(repoName, repoOrg);
+
+                // Keep asking for a valid input if we don't find any matching repo in the given org
+                while (repo == null)
+                {
+                    Console.WriteLine("We couldn't find any matching repository in " + orgName + " organization, please re-enter you repository name.");
+                    Console.WriteLine("If you wan't to leave the program, please type in 'Exit'");
+                    string rName = Console.ReadLine();
+                    if (rName.CompareTo("Exit") == 0)
+                    {
+                        Environment.Exit(0);
+                    }
+                    repo = findRepository(rName, repoOrg);
+                }
+
+
+                // TODO: Now find the given PR then create a check for it
+                Console.WriteLine("repo " + repo.FullName);
+                //client.Check.Suite.Create(user.Login,)
+
+
+            }
+            catch (FileNotFoundException e)
             {
                 Console.WriteLine(e);
                 Environment.Exit(0);
