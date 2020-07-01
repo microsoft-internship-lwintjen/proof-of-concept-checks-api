8d9da87ee74a50aaee153536f94427c309d05abb
lwintjen <loris.wintjens@gmail.com>
2020-07-01 17:33:03 +0200

    Adding a sample branch to be able to create a PR from SampleBranch to Master

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Octokit;
 namespace SampleApp
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+        static async Task Main(string[] args)
         {
-            var github = new GitHubClient(new ProductHeaderValue("SampleApp"));
+            var client = new GitHubClient(new ProductHeaderValue("SampleApp"));
             //github.Check.Suite.Create()
             string tokenPath = @"C:\Windows\System\.env";
+
+            string login = "lwintjen";
+
             DotNetEnv.Env.Load(tokenPath);
             try
             {
@@ -19,11 +26,29 @@ namespace SampleApp
                 {
                     throw new FileNotFoundException("The github token was not found in your .env file, please make sure that the key 'API_GH_PERSONAL_TOKEN' is set in your .env file (C:\\Windows\\System\\.env)");
                 }
+
+                Console.WriteLine("Token found, setting up the token...");
+
+                var tokenAuth = new Credentials(token); 
+                client.Credentials = tokenAuth;
+
+                Console.WriteLine("Finding the current authenticated user...");
+
+                var user = await client.User.Current();
+
+                var repoList = await client.Organization.GetAllForUser(login);
+
+                for (int i = 0; i < repoList.Count; i++)
+                {
+                    Console.WriteLine(repoList.ElementAt(i));
+                }
+                Console.WriteLine("user : " + repoList);
+
+
             } catch (FileNotFoundException e)
             {
                 Console.WriteLine(e);
                 Environment.Exit(0);
-
             }
 
         }
